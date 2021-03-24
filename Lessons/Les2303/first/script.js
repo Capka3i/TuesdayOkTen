39a8f55c29db98c536c50fc6e877d6c1e51b4ece
Capka3i <Capka33i@gmail.com>
2021-03-24 18:55:35 +0200

    Lesons 23.03

---
+++ Lessons/Les2303/first/script.js
@@ -0,0 +1,170 @@
+//1.1
+let hi = 'hello';
+let okten = 'owu';
+let www = 'com';
+let one = 1;
+let ten = 10;
+let evi = -999;
+let value = 123;
+let pi = 3.14;
+let drop = 2.7;
+let beer = 16;
+let ok = true;
+let no = false;
+console.log(hi);
+// alert(hi);
+// document.write(hi);
+//console.log(okten);
+//alert(okten);
+document.write(okten);
+//console.log(www);
+alert(www);
+//document.write(www);
+//console.log(one);
+//alert(one);
+document.write(one);
+console.log(ten);
+//alert(ten);
+//document.write(ten);
+//console.log(evi);
+alert(evi);
+//document.write(evi);
+//console.log(value);
+//alert(value);
+document.write(value);
+console.log(pi);
+//alert(pi);
+//document.write(pi);
+//console.log(drop);
+alert(drop);
+//document.write(drop);
+//console.log(beer);
+//alert(beer);
+document.write(beer);
+console.log(ok);
+//alert(ok);
+//document.write(ok);
+//console.log(no);
+alert(no);
+//document.write(no);
+
+//1.2
+hi = 'HELLO';
+okten = 'ok10';
+www = 'owu.com.ua';
+one = 1.1;
+ten = 1010;
+evi = 25;
+value = -50;
+pi = 3.1415;
+drop = 0.258;
+beer = 1715;
+ok = false;
+no = true;
+console.log(hi);
+// alert(hi);
+// document.write(hi);
+//console.log(okten);
+//alert(okten);
+document.write(okten);
+//console.log(www);
+alert(www);
+//document.write(www);
+//console.log(one);
+//alert(one);
+document.write(one);
+console.log(ten);
+//alert(ten);
+//document.write(ten);
+//console.log(evi);
+alert(evi);
+//document.write(evi);
+//console.log(value);
+//alert(value);
+document.write(value);
+console.log(pi);
+//alert(pi);
+//document.write(pi);
+//console.log(drop);
+alert(drop);
+//document.write(drop);
+//console.log(beer);
+//alert(beer);
+document.write(beer);
+console.log(ok);
+//alert(ok);
+//document.write(ok);
+//console.log(no);
+alert(no);
+//document.write(no);
+
+//1.3
+let name = prompt('Enter name.');
+let fatherName = prompt('Enter your fater name.');
+let age = prompt('Enter your birth.');
+document.write("Вітаю " + name + " " + fatherName + ". Тобі " + age + " років");
+
+//1.4
+
+let all = hi + okten + www + one + ten + evi + value + pi + drop + beer + ok + no;
+document.write(all);
+
+
+//1.5
+
+let firs = parseInt(prompt('Enter first value.'));
+let second = parseInt(prompt('Enter second value.'));
+let therd = parseInt(prompt('Enter therd value.'));
+console.log(`${firs} ${second} ${therd}`)
+
+//1.6
+let firs1 = prompt('Enter first value.');
+let second1 = prompt('Enter second value.');
+let therd1 = prompt('Enter therd value.');
+let forth1 = prompt('Enter forth value.');
+console.log(parseInt(firs1) + parseInt(second1) + parseInt(therd1) + parseInt(forth1));
+
+//1.7
+let firs2 = prompt('Enter first value.');
+let second2 = prompt('Enter second value.');
+let therd2 = prompt('Enter therd value.');
+let result = parseFloat(firs2) + parseFloat(second2) + parseFloat(therd2);
+console.log(result);
+
+//1.8
+let firs3 = Math.round(prompt('Enter first value.'));
+let second3 = Math.round(prompt('Enter second value.'));
+let therd3 = Math.round(prompt('Enter therd value.'));
+let summ;
+summ = firs3 + second3 + therd3;
+console.log(summ);
+
+
+//1.9
+let firs4 = prompt('Enter first value.');
+let second4 = prompt('Enter second value.');
+let pow = Math.pow(parseFloat(firs4), parseInt(second4));
+console.log(pow);
+
+
+//1.10
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+//1.11
+console.log(5 <= 6);
+console.log(5 > 6);
+console.log(5 >= 6);
+console.log(5 === 6);
+console.log(10 >= 10);
+console.log(10 === 10);
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(10 !== 10);
+console.log(123 === '123');
+console.log(123 == '123');
+
