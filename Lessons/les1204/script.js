ff9d24744e588d0ca0035d316e032ee01458d091
Capka3i <Capka33i@gmail.com>
2021-04-13 17:00:57 +0300

    complate1

---
+++ Lessons/les1204/script.js
@@ -0,0 +1,43 @@
+// - Створити довільний елемент с id = text. використовуючи JavaScript, зробіть так, щоб при кліку на кнопку зникавл елемент с id="text".
+// let div = document.createElement('div');
+// let button = document.createElement('button');
+// button.id = 'hidden'
+// button.style.height = '60px';
+// button.style.width = '100px'
+// button.innerHTML = 'Нажми щоб сховати квадрат'
+// button.style.borderColor= 'black';
+// button.style.marginLeft = '160px'
+//
+//
+// div.id = 'text'
+// div.style.height = '400px';
+// div.style.width = '400px';
+// div.style.backgroundColor = 'purple';
+//
+// document.body.appendChild(button);
+// document.body.appendChild(div);
+//
+// document.getElementById('hidden').onclick = function() {
+// 	document.getElementById('text').hidden = true;
+// }
+
+// 	- Створіть кнопку, при кліку на яку, вона буде приховувати сама себе.
+//
+//
+// let button = document.createElement('button');
+// button.id = 'hidden'
+// button.style.height = '60px';
+// button.style.width = '100px'
+// button.innerHTML = 'Нажми щоб сховати квадрат'
+// button.style.borderColor= 'black';
+//
+//
+// document.body.appendChild(button);
+//
+// document.getElementById('hidden').onclick = function() {
+// 	document.getElementById('hidden').hidden = true;
+// }
+
+
+
+
