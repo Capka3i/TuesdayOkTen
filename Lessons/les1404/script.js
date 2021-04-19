268e7f390381b8a1bfe340bdc9c999b1f9bf7e3b
Capka3i <Capka33i@gmail.com>
2021-04-19 16:20:20 +0300

    complate1

--- Lessons/les1404/script.js
+++ Lessons/les1404/script.js
@@ -1,82 +1,82 @@
 //- Дана textarea.
 // В неё вводится текст.
 // Сделайте так, чтобы после захода на эту страницу через некоторое время, введенный текст остался в textarea.
-//
-// const textrea = document.createElement('textarea');
-// document.body.appendChild(textrea);
-// const keys = 'acc';
-// textrea.value = localStorage.getItem(keys)
-// textrea.onchange = ({target:{value}}) => {
-// 	localStorage.setItem(keys,value);
-// }
+
+const texterea = document.getElementById('texterea');
+const keys = 'acc';
+texterea.value = localStorage.getItem(keys)
+texterea.onchange = ({target:{value}}) => {
+	localStorage.setItem(keys,value);
+}
 
 //- Дана форма с инпутами, текстареа, чекбоксами, радио кнопочками, селектами и тп.
 // Пользователь вводит какие-то данные и закрывает страницу (не факт, что он заполнил всю форму).
 // Сделайте так, чтобы при следующем заходе на страницу введенные им ранее данные стояли на своих местах.
 // Сделайте ваш скрипт как можно более универсальным.
 //
-// const forms = document.getElementById('forms');
-// for (const form of forms) {
-// 	if (form.name==='gender'&&localStorage.getItem(form.name) === form.id) {
-// 		form.checked=true;
-// 	} else if ( form.type==='checkbox'){
-// 		form.checked = JSON.parse(localStorage.getItem(form.id));
-// 	} else {
-// 		form.value = localStorage.getItem(form.id)
-// 	}
-// }
-// forms.oninput = ({target: {id, value, type, name, checked}}) => {
-// 	if (type === 'radio' && checked) {
-// 		console.log(checked);
-// 		localStorage.setItem(name, id);
-// 	} else if (type === 'checkbox') {
-// 		localStorage.setItem(id, checked);
-// 	} else {
-// 		localStorage.setItem(id, value)
-// 	}
-// };
+const forms = document.getElementById('forms');
+for (const form of forms) {
+	if (form.name==='gender'&&localStorage.getItem(form.name) === form.id) {
+		form.checked=true;
+	} else if ( form.type==='checkbox'){
+		form.checked = JSON.parse(localStorage.getItem(form.id));
+	} else {
+		form.value = localStorage.getItem(form.id)
+	}
+}
+forms.oninput = ({target: {id, value, type, name, checked}}) => {
+	if (type === 'radio' && checked) {
+		console.log(checked);
+		localStorage.setItem(name, id);
+	} else if (type === 'checkbox') {
+		localStorage.setItem(id, checked);
+	} else {
+		localStorage.setItem(id, value)
+	}
+};
 
 //-Дан текстареа. В него можно ввести данные, нажать кнопку "сохранить" и они "фикисруются" (в хранилище),
 // затем поредактировать их, затем еще поредактировать и возможно еще.....
 // Требование : хранить историю своих изменений (даже после перезагрузки страницы).
 // Сверху над текстареа должны появится стрелочки, с помощью которых можно перемещаться по
 // истории (не забудьте!чекпоинт истории - нажатеи кнопки сохранить).
-//
-// const texterea = document.getElementById('texterea');
-// const save = document.getElementById('save');
-// const but1 = document.getElementById('but1');
-// const but2 = document.getElementById('but2');
-// const STOR = 'arc';
-//
-// if (!localStorage.getItem(STOR)) {
-// 	localStorage.setItem(STOR, JSON.stringify([]));
-// }
-//
-// save.onclick = () => {
-// 	let temp = localStorage.getItem(STOR);
-// 	let acc = JSON.parse(temp);
-// 	acc.push(texterea.value);
-// 	localStorage.setItem(STOR, JSON.stringify(acc));
-// };
-// let i = 0;
-// const s = JSON.parse(localStorage.getItem(STOR));
-//
-// but1.onclick = () => {
-// 	if (!i) {
-// 		i = s.length - 1;
-// 	} else {
-// 		i--;
-// 	}
-// 	texterea.value = s[i];
-// };
-// but2.onclick = () => {
-// 	if (i === s.length - 1) {
-// 		i = 0;
-// 	} else {
-// 		i++;
-// 	}
-// 	texterea.value = s[i];
-// };
+
+const texterea2 = document.getElementById('texterea2');
+const save = document.getElementById('save1');
+const but1 = document.getElementById('but1');
+const but2 = document.getElementById('but2');
+const STOR = 'arc';
+
+if (!localStorage.getItem(STOR)) {
+	localStorage.setItem(STOR, JSON.stringify([]));
+}
+
+save.onclick = () => {
+	let temp = localStorage.getItem(STOR);
+	let acc = JSON.parse(temp);
+	acc.push(texterea2.value);
+	localStorage.setItem(STOR, JSON.stringify(acc));
+};
+let i = 0;
+but1.onclick = () => {
+
+	const s = JSON.parse(localStorage.getItem(STOR));
+	if (!i) {
+		i = s.length - 1;
+	} else {
+		i--;
+	}
+	texterea2.value = s[i];
+};
+but2.onclick = () => {
+	const s = JSON.parse(localStorage.getItem(STOR));
+	if (i === s.length - 1) {
+		i = 0;
+	} else {
+		i++;
+	}
+	texterea2.value = s[i];
+};
 
 // - Реализуйте записную книгу, хранящую данные в локальном хранилище.
 // 	Данные которые надо сохранять : ФИО, номер, почта, фирма, отдел, день рождения
@@ -84,36 +84,40 @@
 // --Каждому контакту добавить кнопку для удаления контакта.
 // --Каждому контакту добавить кнопку редактироваиня. При нажати на нее появляется форма, в которой есть все необходимые
 // инпуты для редактирования, которые уже заполнены данными объекта
-//
-// const forma = document.getElementById('forma');
-// const save = document.getElementById('save');
-// const editing = document.getElementById('editing');
-// const id = document.getElementById('id');
-// let key = 0;
-// save.onclick = () => {
-// 	if (+localStorage.key(key) === key)
-// 		key++;
-// 	let array = [];
-// 	for (const argument of forma) {
-// 		array.push(argument.value);
-//
-// 	}
-//
-// 	localStorage.setItem(key, JSON.stringify(array));
-// 	console.log(array);
-// 	alert(`Your ID ${key}`);
-// 	id.value = key;
-//
-// };
-//
-// editing.onclick = () => {
-// 	let i = 0;
-// 	if (key === +localStorage.key(key)) {
-// 		let acc = JSON.parse(localStorage.getItem(key));
-// 		for (const form of forma) {
-// 			form.value = acc[i];
-// 			i++;
-// 		}
-// 	}
-// 	localStorage.removeItem(key);
-// };
+
+const forma = document.getElementById('forma');
+const save2 = document.getElementById('save2');
+const editing = document.getElementById('editing');
+const id = document.getElementById('id');
+
+save2.onclick = () => {
+	let key = 1;
+	if (+localStorage.key(key) === key)
+		key++;
+	let array = [];
+	for (const argument of forma) {
+		array.push(argument.value);
+
+	}
+
+	localStorage.setItem(key, JSON.stringify(array));
+	console.log(array);
+	alert(`Your ID ${key}`);
+	id.value = key;
+
+};
+
+editing.onclick = () => {
+	let i = 0;
+	console.log(!!localStorage.key(id.value));
+	console.log(+id.value);
+	console.log(JSON.parse(localStorage.getItem(id.value)));
+	if (!!localStorage.key(id.value)) {
+		let acc = JSON.parse(localStorage.getItem(+id.value));
+		for (const form of forma) {
+			form.value = acc[i];
+			i++;
+		}
+	}
+	// localStorage.removeItem(id.value);
+};
--- Lessons/les1704/script.js
+++ Lessons/les1704/script.js
@@ -1,3 +1,38 @@
 //Зробити свій розпорядок дня, використовуючи при цьому лише Callback,Promis та async await.
 // повино бути зроблено не менше 8 умов.
 //
+const numb = prompt('set day');
+function uping(day, up) {
+	setTimeout( () => {
+	if (day<7){
+		console.log('got up now');
+		up(1);
+	} else {
+		console.log('off clock and sleep');
+		up(10,true);
+	}
+	},day*1000)
+}
+function cook(goShop, product){
+	if (!product){
+		goShop();
+	} else {
+		console.log(`cook something ${product}`);
+
+	}
+
+}
+
+uping(numb, (beer, lonelySleep)=>{
+	if (lonelySleep){
+		console.log('hard up');
+		cook(null,`aggs`)
+	}
+
+	setTimeout()
+
+})
+
+function goToShop(money, ){
+
+}
